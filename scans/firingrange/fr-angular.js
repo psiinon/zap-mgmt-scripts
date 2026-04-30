99672d72424994510024ac0e20ba42bb3af17531
Simon Bennetts <psiinon@gmail.com>
2026-04-30 12:11:18 +0200

    FR missing changes
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

--- scans/firingrange/fr-angular.js
+++ scans/firingrange/fr-angular.js
@@ -1,5 +1,5 @@
 var DIR = "/zap/wrk/";
-var NAME = " Angular-based XSSes DOM XSS";
+var NAME = "Angular-based XSSes DOM XSS";
 var TARGET = "angular";
 var RULES = [40012, 200002, 210000, 220000, 220002];
 var MIN_LEVEL = 2;
